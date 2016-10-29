551b8df1bc8e19d95eb31df655022c3db1bd6751
Callum Silcock <csilk@Callums-MacBook-Pro.local>
2016-10-29 12:08:07 +1100

    added eslintrc and default index

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 export default {
   constructor(serverless, options) {
-    this.serverless = serverless;
-    this.service = serverless.service;
-    this.options = options;
+    this.serverless = serverless
+    this.service = serverless.service
+    this.options = options
     this.commands = {
       ses: {
         commands: {
@@ -11,18 +11,17 @@ export default {
             options: {
               port: {
                 shortcut: 'p',
-                usage: 'Specify port for server to run on, default is 9001'
+                usage: 'Specify port for server to run on, default is 9001',
               },
-            }
-          }
-        }
-      }
+            },
+          },
+        },
+      },
     }
     this.hooks = {
       'ses:start:startHandler': this.startHandler.bind(this),
     }
-  }
+  },
   startHandler() {
-    
-  }
+  },
 }
